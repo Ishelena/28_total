c31cba4bba4ae7c528c7d5d08ae424059dfe4486
koclena <koc-lena@mail.ru>
2023-03-27 16:32:17 +0300

    Initial commit

---
+++ settings.py
@@ -0,0 +1,26 @@
+class Settings:
+    chrome_driver = "/tests/chromedriver.exe"
+
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'koc-lena@mail.ru'
+    valid_password = 'Test2023NA4ALO'
+    valid_email_reg = 'Ввести валидный email'
+    invalid_email = 'fdkj@ru.mail'
+    invalid_password = 'fgter645cvbv'
+    empty_email = ''
+    empty_password = ''
+    menu_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    first_name = 'Елена'
+    last_name = 'Кос'
+    first_name_ = '-'
+    last_name_ = '-'
+    russian_generate_string = 'а'
+    latin_generate_string = 'incorrect'
+    chinese_chars = '形龍門大酒家'
+    special_chars = '№;"?@%'
+    numbers = 123
+    empty = ''
+    passw1 = 'Test1'
+    passw2 = 'Test_2'
+    passw3 = 'TEST333'
